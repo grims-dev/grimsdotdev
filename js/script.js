1ccecc7527cf4ca97c30a313106f7031c74faa0a
Ciaran Grimshaw <ciaran.grimshaw@dontpanicprojects.com>
2020-04-08 16:22:22 +0100

    Don't run background animations if user prefers reduced motion

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-let bg = document.querySelector('.background');
+// background animation functionality
 
-let scaleMove = 2.5;
-let scaleMoveHalf = scaleMove / 2;
-let scaleRotate = 4;
-let scaleRotateHalf = scaleRotate / 2;
+// only run if user doesn't have motion settings
+const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+if (prefersReducedMotion === false || prefersReducedMotion === undefined) {
+    let bg = document.querySelector('.background');
 
-window.addEventListener('mousemove', e => {
-    let x = 1 - e.clientX / window.innerWidth;
-    let y = 1 - e.clientY / window.innerHeight;
+    let scaleMove = 2.5;
+    let scaleMoveHalf = scaleMove / 2;
+    let scaleRotate = 4;
+    let scaleRotateHalf = scaleRotate / 2;
 
-    let xMove = (x * scaleMove) - scaleMoveHalf;
-    let yMove = (y * scaleMove) - scaleMoveHalf;
+    window.addEventListener('mousemove', e => {
+        let x = 1 - e.clientX / window.innerWidth;
+        let y = 1 - e.clientY / window.innerHeight;
 
-    let xRotate = (x * scaleRotate) - scaleRotateHalf;
-    let yRotate = (y * scaleRotate) - scaleRotateHalf;
+        let xMove = (x * scaleMove) - scaleMoveHalf;
+        let yMove = (y * scaleMove) - scaleMoveHalf;
 
-    window.requestAnimationFrame(function() {
-        bg.style.transform = `
+        let xRotate = (x * scaleRotate) - scaleRotateHalf;
+        let yRotate = (y * scaleRotate) - scaleRotateHalf;
+
+        window.requestAnimationFrame(function() {
+            bg.style.transform = `
             translateX(${xMove}em)
             translateY(${yMove}em)
             rotateX(${xRotate}deg)
             rotateY(${yRotate}deg)
         `;
+        });
     });
-});
+}
